4afad753c6045f0a7a9877a8d4d4d370fa1aa885
Uri Gil <uri@hilma.tech>
2024-08-03 11:53:31 +0300

    changed text to simulate feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React && Docker!
+          Learn React && Docker - New feature 
         </a>
       </header>
     </div>
